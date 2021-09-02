48a88f6baaa00496e44d33672e757c8a682316a6
Elnaz <e.pouranbarani@gmail.com>
2021-09-02 15:15:17 -0400

    Updated code

--- scraping.py
+++ scraping.py
@@ -6,14 +6,32 @@ import pandas as pd
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
+import datetime as dt
 
-# Set your executable path 
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+def scrape_all():
+    # # Set executable path , Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True) # Run headless mode: scraping is accomplished, but behind the scenes.
+    
+    news_title, news_paragraph = mars_news(browser)
+
+# Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+       # Stop webdriver and return data
+    browser.quit()
+    return data
+
 
 def mars_news(browser):
     # Visit the mars nasa news site
-    url = 'https://redplanetscience.com'
+    #url = 'https://redplanetscience.com'
+    url ='https://data-class-mars.s3.amazonaws.com/Mars/index.html'
     browser.visit(url)
 
     # Optional delay for loading the page
@@ -41,8 +59,10 @@ def mars_news(browser):
     
         # Use the parent element to find the paragraph text
         news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    
     except AttributeError:
         return None, None
+    
     return news_title, news_p
 
 
@@ -50,7 +70,8 @@ def mars_news(browser):
 def featured_image(browser):
       # # Scrape Mars Data: Image
     #Visit URL
-    url = 'https://spaceimages-mars.com'
+    #url = 'https://spaceimages-mars.com'
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
     browser.visit(url)
 
     # Find and click the full image button
@@ -68,22 +89,32 @@ def featured_image(browser):
     
     except AttributeError:
          return None
+
     # Use the base URL to create an absolute URL
-    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+    #img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+    img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
+    
     return img_url
 
 
 def mars_facts():
+     # Add try/except for error handling
     try:
         # Create DataFrame from HTML table, pull only the first table.
-        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+        #df = pd.read_html('https://galaxyfacts-mars.com')[0]
+        df = pd.read_html('https://data-class-mars-facts.s3.amazonaws.com/Mars_Facts/index.html')[0]
+
     except BaseException:
       return None
+      
     df.columns=['description', 'Mars', 'Earth']
     df.set_index('description', inplace=True)
-    return df.to_html()
+    return df.to_html(classes="table table-striped")
+    
+# Tell flask the script is complete and ready for action
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
 
-    #browser.quit()
 
-
-
+ # type python app.py in terminal in environmnet 
