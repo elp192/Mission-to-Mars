57c46839f2f9ea30143fb29ed86d037d83e92561
Elnaz <e.pouranbarani@gmail.com>
2021-09-02 11:07:00 -0400

    Set up scraping route

--- app.py
+++ app.py
@@ -15,9 +15,18 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 # Define route for the HTML page
-@app.route("/")  # what to display when we're looking at the home page
+@app.route("/")  # what to display when we're looking at the home page (route that Flask used)
 def index():
    mars = mongo.db.mars.find_one() # Use Pymongo to find the mars collection in database
    return render_template("index.html", mars=mars) # Use mars collection
 
+# Setup Scrapping route"
+@app.route("/scrape")
+def scrape():
+   mars = mongo.db.mars # Assign a new variable that points to Mongo database
+   mars_data = scraping.scrape_all() # Create a new variable to hold the newly scraped data (referencing the scrape_all function in the scraping.py file exported from Jupyter Notebook)
+   mars.update({}, mars_data, upsert=True) # Add an empty JSON object with {}, upsert=True indicates to Mongo to create a new document if one doesn't already exist
+   return redirect('/', code=302) # Navigate our page back to / where we can see the updated content
 
+   if __name__ == "__main__":
+      app.run()
